c6ec2c304c4ae242a44006aac0f232361343a366
Anis95150 <32640183+Anis95150@users.noreply.github.com>
2018-03-22 16:00:56 +0100

    Pour me retrouver dans mes mangas

---
+++ app.js
@@ -0,0 +1,74 @@
+var init = function(){
+
+var btn = document.querySelector(".btn");
+var btns = document.querySelector(".btns");
+var liste = document.querySelector(".liste");
+var fini = document.querySelector(".fini");
+
+
+btn.onclick = function(){
+    if(liste.style.visibility == "visible"){
+        liste.style.visibility = "hidden";
+    }
+
+    else{
+        liste.style.visibility = "visible"
+    }
+    
+};
+
+btns.onclick = function(){
+    if(fini.style.visibility == "visible"){
+        fini.style.visibility = "hidden";
+    }
+
+    else{
+        fini.style.visibility = "visible";
+    }
+};
+}
+
+var liste_mangas = ["Hunter x Hunter ",
+    "My Hero Academia ",
+    "One piece ",
+    "Black Clover ",
+    "Shingeki no Kyojin ",
+    "Berserk ",
+    "Nanatsu no Taizai ",
+    "The Promised ",
+    "One punch man ",
+    "Kingdom ",
+    "Made in abyss ",
+    "Ippo ",
+    "Tokyo Ghoul ",
+    "JoJO Bizzare adventure ",
+    "Full metal alchimiste ",
+    "Detective Conan ",
+    "Sun Ken Rock ",
+    "Bleach ",
+    "Naruto ",
+    "Dragon ball Z ", 
+    "Fairy Tail ",
+    "Death Note ",
+    "Eyeshield 21 ",
+    "Ken le survivant ",
+    "Les Chevaliers du Zodiac ",
+    "Olive et Tom ",
+    "Beelzebub ",
+    "Shaman King "]
+
+function filtreTexte(requete) {
+	return liste_mangas.filter(function(el) {
+		return el.toUpperCase().indexOf(requete.toUpperCase()) > -1;
+	})
+}
+
+function mangas(){
+
+var input = document.getElementById("texte").value; 
+document.getElementById("para").innerHTML ="Les mangas qui contiennent "+ input +" sont :"+ filtreTexte(input);
+}
+
+
+window.addEventListener("DOMContentLoaded", init)
+
